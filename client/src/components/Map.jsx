4ef3c3a05a245728be340506ab75603d6c869b6e
Evan Robinson <erob7856@gmail.com>
2023-01-29 01:17:20 -0500

    markers won't work pls help

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import * as React from 'react';
 
 const containerStyle = {
@@ -18,7 +18,10 @@ export default function Map() {
     });
 
     const Ohio = {lat:40.4173,lng:82.9071};
-
+    const Maine = {lat:45.2538,lng:69.4455};
+    const Washington = {lat:47.7511,lng:120.7401};
+    const Florida = {lat:27.6648,lng:81.5158};
+    const Texas = {lat:31.9686,lng:99.9018};
 
     if (!isLoaded) return (<div>Loading...</div>);
     else return(
@@ -27,11 +30,11 @@ export default function Map() {
             mapContainerStyle={containerStyle}
             center={center}
         >
-            <MarkerF position={Ohio} />
-        </GoogleMap>);
-
-    // const Maine = useMemo(() => ({lat:45.2538,lng:69.4455}), []);
-    // const Washington = useMemo(() => ({lat:47.7511,lng:120.7401}), []);
-    // const Florida = useMemo(() => ({lat:27.6648,lng:81.5158}), []);
-    // const Texas = useMemo(() => ({lat:31.9686,lng:99.9018}), []);
+            <Marker position={Ohio} />
+            <Marker position={Maine} />
+            <Marker position={Washington} />
+            <Marker position={Florida} />
+            <Marker position={Texas} />
+        </GoogleMap>
+    );
 }
