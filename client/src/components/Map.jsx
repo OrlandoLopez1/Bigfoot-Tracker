79ba88bd0b8aa64bfdec591aad99a7445670f2aa
Coomesicle <stephencoomes@ufl.edu>
2023-01-29 02:52:24 -0500

    pleasework

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,4 +1,5 @@
 import { GoogleMap, MarkerF, useJsApiLoader, MarkerLabel } from '@react-google-maps/api';
+import { within } from '@testing-library/react';
 import * as React from 'react';
 
 const containerStyle = {
@@ -18,12 +19,34 @@ export default function Map() {
         googleMapsApiKey: 'AIzaSyC9vwgqv8LdmoZTw4coPyaqd6MRdDLJkJo',
     });
 
-    const Michigan = {lat:44.3148,lng:-85.6024};
-    const Virginia = {lat:37.4316,lng:-78.6569};
-    const Illinois = {lat:40.6331 ,lng:-89.3985};
-    const Florida = {lat:27.6648,lng:-81.5158};
-    const Texas = {lat:31.9686,lng:-99.9018};
+    const MI = {lat:44.3148,lng:-85.6024};
+    const VA = {lat:37.4316,lng:-78.6569};
+    const IL = {lat:40.6331 ,lng:-89.3985};
+    const FL = {lat:27.6648,lng:-81.5158};
+    const TX = {lat:31.9686,lng:-99.9018};
+    const TN = {lat:35.5175 ,lng:-86.5804};
+    const GA = {lat:32.1656 ,lng:- 82.9001};
+    const WV = {lat:38.5976 ,lng:-80.4549};
+    const WI = {lat:43.7844 ,lng:-88.7879};
+    const IN = {lat:40.2672 ,lng:-86.1349};
+    const LA = {lat:30.9843 ,lng:-91.9623};
+    /*const New_York = {lat:43.2994 ,lng:-74.2179};
     const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    const New_York = {lat:43.2994 ,lng:-74.2179};
+    */
 
     if (!isLoaded) return (<div>Loading...</div>);
     else return(
@@ -32,12 +55,17 @@ export default function Map() {
             mapContainerStyle={containerStyle}
             center={center}
         >
-            <MarkerF position={Michigan} label='1' text="yo" />
-            <MarkerF position={Texas} label='2'/>
-            <MarkerF position={Virginia} label='3' />
-            <MarkerF position={Florida} label='4'/>
-            <MarkerF position={Illinois} label='5'/>
-            <MarkerF position={New_York} label='6'/>
+            <MarkerF position={MI} label='1' text="yo" />
+            <MarkerF position={TX} label='2'/>
+            <MarkerF position={VA} label='3' />
+            <MarkerF position={FL} label='4'/>
+            <MarkerF position={IL} label='5'/>
+            <MarkerF position={TN} label='6'/>
+            <MarkerF position={GA} label='7'/>
+            <MarkerF position={WV} label='8'/>
+            <MarkerF position={WI} label='9'/>
+            <MarkerF position={IN} label='10'/>
+            <MarkerF position={LA} label='11'/>
         </GoogleMap>
     );
 }
