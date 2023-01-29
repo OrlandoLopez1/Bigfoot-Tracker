b7c85211abd7b5c37f41f1933dbd4f1baf47673f
erobx <erob7856@gmail.com>
2023-01-28 20:12:38 -0500

    add css to frontpage

---
+++ client/src/components/Frontpage.jsx
@@ -0,0 +1,50 @@
+import { AppBar, CssBaseline, Toolbar, Typography, Box, createTheme, ThemeProvider } from "@mui/material";
+import { Container } from "@mui/system";
+import * as React from 'react';
+import './Frontpage.css'
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+function Frontpage() {
+    return(
+    <>
+        <ThemeProvider theme={darkTheme}>
+        <CssBaseline>
+        <AppBar position="relative" className="top-bar">
+            <Toolbar>
+                <Typography variant="h6">
+                    Big Foot Tracker
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <main>
+            <div>
+                <Container maxWidth="sm" className="container">
+                    <Typography className="top-header" variant="h2" align="center" color="textPrimary" gutterBottom>
+                        Where He Been?
+                    </Typography>
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        Have you ever wondered where Big Foot has traveled?
+                    </Typography>
+                    <div>
+                        
+                    </div>
+                </Container>
+            </div>
+        </main>
+        <footer className="footer">
+            <Typography variant="h6" align="center" gutterBottom>
+                Made by: Lando, Stephen, Noah and Evan
+            </Typography>
+        </footer>
+        </CssBaseline>
+        </ThemeProvider>
+    </>
+    );  
+}
+
+export default Frontpage;
