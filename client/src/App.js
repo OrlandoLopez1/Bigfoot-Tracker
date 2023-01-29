194c59814551c524cd4d15b6a36fde6ce2724af8
erobx <erob7856@gmail.com>
2023-01-28 19:02:57 -0500

    reorganize structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Frontpage from './components/Frontpage';
+
+
+const App = () => {
+  return (
+    <div style={{ backgroundColor: 'black', className:'BigfootTracker'}}>
+      <Frontpage />
+    </div>
+  )
+}
+
+export default App;
