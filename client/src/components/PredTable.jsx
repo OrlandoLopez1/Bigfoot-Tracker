2c847151aaf0a3184b7d967be102de2ac09edb40
NoahJohnson <87385416+NoahbJohnson15@users.noreply.github.com>
2023-01-29 02:39:36 -0500

    Update PredTable.jsx

--- client/src/components/PredTable.jsx
+++ client/src/components/PredTable.jsx
@@ -1,65 +1,76 @@
 import * as React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, tableCellClasses, styled } from '@mui/material'
-import './PredTable.css'
+import { DataGrid } from '@mui/x-data-grid';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
+const columns = [
+  { field: 'id', headerName: 'State', width: 105 },
+  { field: 'percentage', headerName: 'Chance of Big Foot', width: 135 },
   
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
-
-function createData(state, percentage) {
-  return { state, percentage};
-}
-
-const rows = [
-  createData('Michigan', 10 + '%'),
-  createData('Texas', 8 + '%'),
-  createData('Virginia', 7 + '%'),
-  createData('Florida', 5 + '%'),
-  createData('Illinois', 5 + '%'),
-  createData('New York', 5 + '%')
+ 
+  
 ];
 
-export default function PredTable() {
+const rows = [
+  { id: 'Michigan', percentage: 10 + '%'},
+  { id: 'Texas', percentage: 8 + '%'},
+  { id: 'Virginia', percentage: 7 + '%' },
+  { id: 'Florida', percentage: 5 + '%'},
+  { id: 'Illinois', percentage: 5 +'%' },
+  { id: 'New York', percentage: 5 + '%'},
+  { id: 'Tennessee', percentage: 5 + '%' },
+  { id: 'Georgia', percentage: 4 + '%' },
+  { id: 'West Virginia', percentage: 4 + '%'},
+  { id: 'Wisconsin', percentage: 4 + '%'},
+  { id: 'Indiana', percentage: 3 + '%'},
+  { id: 'Louisiana', percentage: 3 + '%' },
+  { id: 'Missouri', percentage: 3 + '%'},
+  { id: 'Montana', percentage: 3 + '%' },
+  { id: 'Ohio', percentage: 3 +'%' },
+  { id: 'Alabama', percentage: 2 + '%'},
+  { id: 'Arkansas', percentage: 2 + '%' },
+  { id: 'Colorado', percentage: 2 + '%' },
+  { id: 'Kentucky', percentage: 2 + '%' },
+  { id: 'Nevada', percentage: 2 + '%' },
+  { id: 'North Carolina', percentage: 2 + '%' },
+  { id: 'Pennsylvania', percentage: 2 + '%' },
+  { id: 'Washington', percentage: 2 + '%' },
+  { id: 'Alaska', percentage: 1 + '%' },
+  { id: 'California', percentage: 1 + '%' },
+  { id: 'Iowa', percentage: 1 + '%' },
+  { id: 'Kansas', percentage: 1 + '%' },
+  { id: 'Maryland', percentage: 1 + '%' },
+  { id: 'Michigan', percentage: 1 + '%' },
+  { id: 'Mississippi', percentage: 1 + '%' },
+  { id: 'New Hampshire', percentage: 1 + '%' },
+  { id: 'North Dakota', percentage: 1 + '%' },
+  { id: 'Oklahoma', percentage: 1 + '%' },
+  { id: 'Vermont', percentage: 1 + '%' },
+  { id: 'Wyoming', percentage: 1 + '%' },
+  { id: 'Arizona', percentage: 0 + '%' },
+  { id: 'Connecticut', percentage: 0 + '%' },
+  { id: 'Delaware', percentage: 0 + '%' },
+  { id: 'Hawaii', percentage: 0 + '%' },
+  { id: 'Idaho', percentage: 0 + '%' },
+  { id: 'Maine', percentage: 0 + '%' },
+  { id: 'Massachusetts', percentage: 0 + '%' },
+  { id: 'Minnesota', percentage: 0 + '%' },
+  { id: 'Newbraska', percentage: 0 + '%' },
+  { id: 'New Jersey', percentage: 0 + '%' },
+  { id: 'New Mexico', percentage: 0 + '%' },
+  { id: 'Oregon', percentage: 0  + '%' },
+  { id: 'Rhode Island', percentage: 0 + '%' },
+  { id: 'South Dakota', percentage: 0 + '%' },
+  { id: 'Utah', percentage: 0 + '%' },
+];
+
+export default function DataTable() {
   return (
-    
-    <TableContainer className= "table-header" component={Paper}>
-      <Table sx={{ minWidth: 100 }} aria-label="simple table">
-        <TableHead>
-          <StyledTableRow>
-            <StyledTableCell>State</StyledTableCell>
-            <StyledTableCell align="right">Chance of Big Foot?</StyledTableCell>
-          </StyledTableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow
-              key={row.state}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <StyledTableCell component="th" scope="row">
-                {row.state}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.percentage}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div style={{ height: 400, width: '150%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+      />
+    </div>
   );
 }
